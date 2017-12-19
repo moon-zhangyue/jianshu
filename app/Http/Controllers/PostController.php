d492e2b51d829afbc0352efcef6292015584df22
[moon-zhangyue] <[575865770@qq.com]>
2017-12-19 22:37:58 +0800

    更新路由

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    //文章列表页
+    public function index()
+    {
+
+    }
+
+    //文章详情页
+    public function show()
+    {
+
+    }
+
+    //创建文章
+    public function create()
+    {
+
+    }
+
+    //创建逻辑
+    public function store()
+    {
+
+    }
+
+    //编辑页面
+    public function edit()
+    {
+
+    }
+
+    //编辑逻辑
+    public function update()
+    {
+
+    }
+
+    //删除逻辑
+    public function delete()
+    {
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,29 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+
+Route::get('/', "\App\Http\Controllers\LoginController@index");  //首页
+
+
+Route::get('/posts','\App\Http\Controllers\PostController@index');//文章列表页
+//创建文章
+Route::get('/posts/create', '\App\Http\Controllers\PostController@create');
+Route::post('/posts', '\App\Http\Controllers\PostController@store');
+
+Route::get('/posts/search', '\App\Http\Controllers\PostController@search');
+Route::get('/posts/{post}', '\App\Http\Controllers\PostController@show');//文章详情页
+//编辑文章
+Route::get('/posts/{post}/edit', '\App\Http\Controllers\PostController@edit');
+Route::put('/posts/{post}', '\App\Http\Controllers\PostController@update');
+
+//删除文章
+Route::get('/posts/delete', '\App\Http\Controllers\PostController@delete');
+
+Route::post('/posts/img/upload', '\App\Http\Controllers\PostController@imageUpload');
+Route::post('/posts/comment', '\App\Http\Controllers\PostController@comment');
+Route::get('/posts/{post}/zan', '\App\Http\Controllers\PostController@zan');
+Route::get('/posts/{post}/unzan', '\App\Http\Controllers\PostController@unzan');
