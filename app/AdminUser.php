9912fca12e28b0bbede112c58d12d3219da38c20
moon-zhangyue <575865770@qq.com>
2018-05-17 18:10:53 +0800

    用户权限管理

--- app/AdminUser.php
+++ app/AdminUser.php
@@ -33,4 +33,36 @@ class AdminUser extends Authenticatable
     {
         return $this->belongsToMany(\App\AdminUser::class, 'admin_role_user', 'user_id', 'role_id')->withPivot(['user_id', 'role_id']);
     }
+
+    /*
+     * 判断是否有某个角色,某些角色
+     * */
+    public function isInRoles($roles)
+    {
+        return !!$roles->intersect($this->roles)->count();
+    }
+
+    /*
+     * 给用户分配角色
+     * */
+    public function assignRole($role)
+    {
+        return $this->roles()->save($role);
+    }
+
+    /*
+     * 取消用户分配的角色
+     * */
+    public function deleteRole($role)
+    {
+        return $this->roles()->detach($role);
+    }
+
+    /*
+     * 用户是否有权限
+     * */
+    public function hasPermission($permission)
+    {
+        //TODO
+    }
 }
